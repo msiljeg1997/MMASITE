cefeebacffedd8e7cfcce8abb176702820c81350
Matija <95matija@gmail.com>
2024-04-08 14:41:38 +0200

    promjene

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  backgroundSize: string = '980px';
+  isMobile = window.innerWidth < 450;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const target = event.target as Window | null;
+    if (target) {
+      const viewportWidth = target.innerWidth;
+      this.backgroundSize =
+        viewportWidth > 980 ? `${viewportWidth}px auto` : '980px auto';
+    }
+  }
+
+  ngOnInit() {
+    this.backgroundSize =
+      window.innerWidth > 980 ? `${window.innerWidth}px auto` : '980px auto';
+  }
 }
--- src/app/za-djecu/za-djecu.component.ts
+++ src/app/za-djecu/za-djecu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-za-djecu',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./za-djecu.component.scss']
 })
 export class ZaDjecuComponent {
+  backgroundSize: string = '980px';
+  isMobile = window.innerWidth < 450;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const target = event.target as Window | null;
+    if (target) {
+      const viewportWidth = target.innerWidth;
+      this.backgroundSize =
+        viewportWidth > 980 ? `${viewportWidth}px auto` : '980px auto';
+    }
+  }
+
+  ngOnInit() {
+    this.backgroundSize =
+      window.innerWidth > 980 ? `${window.innerWidth}px auto` : '980px auto';
+  }
 }
